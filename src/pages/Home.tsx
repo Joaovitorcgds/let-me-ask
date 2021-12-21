970b9d3c4f476d45ca177fcf62910716ab06bc95
João Vitor <joaovitorcgds@gmail.com>
2021-12-21 01:45:02 -0300

    removi o navigate

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Navigate, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
  
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
